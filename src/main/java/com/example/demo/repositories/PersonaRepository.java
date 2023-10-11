51db9b1a4beb69bcc36a61a14d387549b20694c8
Federico Ibañez <federicosjrc55@gmail.com>
2023-10-11 13:17:05 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/repositories/PersonaRepository.java
@@ -0,0 +1,45 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Persona;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonaRepository extends BaseRepository<Persona, Long> {
+
+    //Anotacion metodo de query
+    List<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido);
+
+    Page<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido, Pageable pageable);
+    boolean existsByDni(int dni);
+
+    //ANOTACION JPQL USANDO PARAMETROS INDEXADOS
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE '%?1%' OR p.apellido LIKE '%?1%'")
+    List<Persona> search(String filtro);
+
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE '%?1%' OR p.apellido LIKE '%?1%'")
+    Page<Persona> search(String filtro, Pageable pageable);
+
+    //ANOTACION JPQL USANDO PARAMETROS NOMBRADOS
+   // @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE '%:filtro%' OR p.apellido LIKE '%:filtro%'")
+    //List<Persona> search2(@Param("filtro")String filtro);
+
+    //NATIVE QUERY
+    @Query(
+            value = "SELECT * FROM persona WHERE persona.nombre LIKE '%:filtro%' OR persona.apellido LIKE '%:filtro%'",
+            nativeQuery = true
+    )
+    List<Persona> search3(String filtro);
+
+    @Query(
+            value = "SELECT * FROM persona WHERE persona.nombre LIKE '%:filtro%' OR persona.apellido LIKE '%:filtro%'",
+            countQuery = "SELECT count(*) FROM persona",
+            nativeQuery = true
+    )
+    Page<Persona> search3(String filtro, Pageable pageable  );
+}
